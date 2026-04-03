1b42752b82b0bc1025cf092e39f38526954f65ce
shakilook <shakil.sabser@gmail.com>
2026-04-03 09:56:15 +0600

    Update video2.js

--- scripts/cmds/video2.js
+++ scripts/cmds/video2.js
@@ -1,4 +1,4 @@
-!cmd install video2.js const axios = require("axios");
+ const axios = require("axios");
 const fs = require("fs-extra");
 const path = require("path");
 const ytSearch = require("yt-search");
